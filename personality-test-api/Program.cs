1d27ce4e11e818bcf9d5977947d15ddac6358b9a
Bala L <balaji.lakkusamy@gmail.com>
2022-09-06 19:23:14 +0530

    added endpoints for Question Administration

--- personality-test-api/Program.cs
+++ personality-test-api/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.AspNetCore.Diagnostics;
+using personality_test_api.Db.Connections;
+using personality_test_api.Db.Repositories;
+using personality_test_api.Services;
+using personality_test_api.Util;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +13,27 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddEntityFrameworkSqlite().AddDbContext<AppDb>();
+
+builder.Services.AddScoped<IQuestionRepository, QuestionRepository>();
+builder.Services.AddScoped<IQuestionOptionRepository, QuestionOptionRepository>();
+builder.Services.AddScoped<IQuestionManager, QuestionManager>();
+
 var app = builder.Build();
+
+app.UseExceptionHandler(e => e.Run(async context =>
+{
+    var pathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+    var ex = pathFeature.Error;
+
+    if(typeof(CustomBadRequest) == ex.GetType())
+        context.Response.StatusCode = 400;
+    else if(typeof(CustomNotFound) == ex.GetType())
+        context.Response.StatusCode = 404;
+
+    context.Response.ContentType = "application/json";
+    await context.Response.WriteAsync($"Exception Occurred : {ex.Message}");
+}));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
